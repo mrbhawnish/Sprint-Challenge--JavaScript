61f8ed6994a1d3fa91d2235c69745de77f86d276
bhawnishk1 <mrbhawnish@gmail.com>
2020-05-15 18:51:16 -0700

    closures done

--- challenges/closure.js
+++ challenges/closure.js
@@ -6,19 +6,30 @@
 const external = "I'm outside the function";
 
 function myFunction() {
-  console.log(external);
+ // console.log(external);
   const internal = "Hello! I'm inside myFunction!";
 
   function nestedFunction() {
-    console.log(internal);
+   // console.log(internal);
   };
   nestedFunction();
 }
 myFunction();
 
 // Explanation: 
-
+//Because nested function is an inner-function and thus, it's a closure which has access to any variables declared in its parent function.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num){
+let counter = 0;
+for(num; num > 0; num--){
+  counter = counter + num
+}
+return counter;
+}
+
+// console.log(summation(4))
+
